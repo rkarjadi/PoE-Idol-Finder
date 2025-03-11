daeb1f7ab1d06faee273cfdeac691232f3f479e7
Ryan Karjadi <rkarjadi@bu.edu>
2025-03-10 22:01:49 -0400

    moved requirements

--- backend/app.py
+++ backend/app.py
@@ -305,4 +305,5 @@ def get_idols_with_content_tags(stash_id):
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    # app.run(host="localhost", port=5000, debug=True)
+    app.run()
