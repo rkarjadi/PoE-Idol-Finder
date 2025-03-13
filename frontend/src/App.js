baa1d1ab1b91d30ecad0a9de7125daa480771430
Ryan Karjadi <rkarjadi@bu.edu>
2025-03-12 23:38:53 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css'; // Import the CSS file
 
 const App = () => {
-  // const [isAuthorized, setIsAuthorized] = useState(false);
+  const [isAuthorized, setIsAuthorized] = useState(false);
   const [stashes, setStashes] = useState([]);
   const [selectedStash, setSelectedStash] = useState('');
   const [stashesFetched, setStashesFetched] = useState(false);
@@ -11,25 +11,25 @@ const App = () => {
   const [selectedContentTag, setSelectedContentTag] = useState('');
   
   useEffect(() => {
-    // const checkAuthorization = async () => {
-    //   const response = await fetch("https://poe-idol-finder.onrender.com/is_authorized");
-    //   const data = await response.json();
+    const checkAuthorization = async () => {
+      const response = await fetch("https://poe-idol-finder.onrender.com/is_authorized");
+      const data = await response.json();
 
-    //   if (data.authorized) {
-    //     setIsAuthorized(true);
-    //     localStorage.setItem("isAuthorized", true);
-    //   } else {
-    //     localStorage.removeItem("isAuthorized");
-    //     window.location.href = 'https://poe-idol-finder.onrender.com/authorize';
-    //   }
-    // };
+      if (data.authorized) {
+        setIsAuthorized(true);
+        localStorage.setItem("isAuthorized", true);
+      } else {
+        localStorage.removeItem("isAuthorized");
+        window.location.href = 'https://poe-idol-finder.onrender.com/authorize';
+      }
+    };
 
-    // if (isAuthorized) {
-    //   console.log("You are authorized.")
-    // } else {
-    //   checkAuthorization();
-    // }
-    window.location.href = 'https://poe-idol-finder.onrender.com/is_authorized'
+    if (isAuthorized) {
+      console.log("You are authorized.")
+    } else {
+      checkAuthorization();
+    }
+    // window.location.href = 'https://poe-idol-finder.onrender.com/is_authorized'
     const handleBeforeUnload = () => {
       navigator.sendBeacon('/logout');
     };
@@ -82,13 +82,13 @@ const App = () => {
         .sort((a, b) => b.contentTags[selectedContentTag] - a.contentTags[selectedContentTag])
     : stashIdols;
 
-  // if (!isAuthorized) {
-  //   return (
-  //     <div>
-  //       Redirecting to Authorization Page
-  //     </div>
-  //   );
-  // }
+  if (!isAuthorized) {
+    return (
+      <div>
+        Redirecting to Authorization Page
+      </div>
+    );
+  }
 
   return (
     <div className="center">
