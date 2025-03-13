32f5c70d3281a9ac0cc4c6e87985060ed2ad83c9
Ryan Karjadi <rkarjadi@bu.edu>
2025-03-13 13:39:39 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -233,6 +233,7 @@ def oauth_callback():
     session['access_token'] = tokens.get("access_token")
     session['refresh_token'] = tokens.get('refresh_token')
     print(f"Session token set: {session['access_token']}")
+    print(f"Session: {session}")
     # Redirect to home page
     return redirect("https://poe-idol-finder-1.onrender.com")
 
